2ff19fac0473bb5cc601930253ec153ad908817c
Jbsmall17 <alaomiyaki17@gmail.com>
2023-09-04 11:48:47 -0700

    added video at the background

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -3,7 +3,7 @@ import "../Styles/landingpage.css"
 import Navbar from '../components/Navbar'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBarsStaggered,faPlus,faMinus, faXmark} from '@fortawesome/free-solid-svg-icons'
-
+import Video from '../assets/video.mp4'
 
 export default function LandingPage() {
     const [isNavbarOpened, setIsNavbarOpened] = useState(false);
@@ -18,9 +18,9 @@ export default function LandingPage() {
             ? <FontAwesomeIcon onClick={()=>{setIsNavbarOpened(true)}} icon={faBarsStaggered} size="2xl"  className='svg-icon'/>
             : <FontAwesomeIcon onClick={()=>{setIsNavbarOpened(false)}} icon={faXmark} size="2xl"  className='svg-icon xmark'/>
         }
-        <video autoplay>
+        <video autoplay muted>
             <source 
-                src="https://eadn.co.uk/wp-content/uploads/2023/07/EADN-Website-2-LOW-3.mp4" 
+                src={Video} 
                 type="video/mp4"
             />
             Your browser does not support the video tag.
